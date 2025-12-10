416f57709e0114f7a09816b10636444cf014f076
Vedant Saini <72696670+VedantSaini12@users.noreply.github.com>
2025-12-10 00:15:43 +0000

    task 2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import csv
 df0 = pd.read_csv("data/daily_sales_data_0.csv")
 df1 = pd.read_csv("data/daily_sales_data_1.csv")
 df2 = pd.read_csv("data/daily_sales_data_2.csv")
@@ -8,4 +9,5 @@ df = df[df["product"] == "pink morsel"]
 df['price'] = df['price'].str[1:].astype(float)
 df["sales"] = df["quantity"] * df["quantity"]
 df.drop(columns=["price", "quantity", "product"], axis=1, inplace=True)
-print(df)
+
+df.to_csv("formatted.csv", index=False)
