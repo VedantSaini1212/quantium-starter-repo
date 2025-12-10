75094236c3eba56ff756bbd81b02b4faba603971
Vedant Saini <72696670+VedantSaini12@users.noreply.github.com>
2025-12-10 00:09:07 +0000

    task 2

---
+++ app.py
@@ -0,0 +1,11 @@
+import pandas as pd
+df0 = pd.read_csv("data/daily_sales_data_0.csv")
+df1 = pd.read_csv("data/daily_sales_data_1.csv")
+df2 = pd.read_csv("data/daily_sales_data_2.csv")
+
+df = pd.concat([df0, df1, df2])
+df = df[df["product"] == "pink morsel"]
+df['price'] = df['price'].str[1:].astype(float)
+df["sales"] = df["quantity"] * df["quantity"]
+df.drop(columns=["price", "quantity", "product"], axis=1, inplace=True)
+print(df)
